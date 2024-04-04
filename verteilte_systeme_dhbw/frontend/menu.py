b6aaccc2e1d504eeeb22c15c8595bfc21b82890a
Eisebrain <inf21208@lehre.dhbw-stuttgart.de>
2024-04-04 14:10:18 +0200

    menu

--- verteilte_systeme_dhbw/frontend/menu.py
+++ verteilte_systeme_dhbw/frontend/menu.py
@@ -2,7 +2,9 @@ import requests
 from select_answer import *
 import os
 
-
+url = 'http://127.0.0.1:8000'
+url1 = 'http://127.0.0.1:8000/connect/'
+url2 = 'http://127.0.0.1:8000/question/'
 
 while True:
     # menu for user
@@ -12,28 +14,21 @@ while True:
     print("****************************************")
     print("\n")
     print("- You're going to get questions and you have to answer them by choosing between the options a, b or c.")
-    print("- On the top of the screen you can see the current level and the number of correct and wrong answers.")
+    print("- On the top of the screen you can see the current level and the number of correct answers.")
     print("- The difficulty level of the questions will increase with every correct answer. False answers are not "
           "going to cost you levels.")
-    print("- You can increase or decrease your level manually by pressing L.")
     print("\n")
+    user_input = input("Press S (Start) or Q(Quit)")
 
-    while True:
+    if user_input == "S":
+        # clear the console
+        print("\n" * 100)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        start_game()
 
-        user_input = input("Press S (Start) or Q(Quit)")
-
-        if user_input == "S":
-            # clear the console
-            print("\n" * 100)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            start_game()
-
-        elif user_input == "Q":
-            print("Goodbye!")
-            exit()
-        else:
-            print("Please enter a valid input!")
-            continue
+    elif user_input == "Q":
+        print("Goodbye!")
+        exit()
 
 
 
