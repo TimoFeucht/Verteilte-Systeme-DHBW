f78ab4db9d4d8e997bf3d928ba91a5282e2e41ba
Eisebrain <inf21208@lehre.dhbw-stuttgart.de>
2024-04-04 15:52:32 +0200

    Added every api call, caught errors correct/wrong answer counter missing

--- verteilte_systeme_dhbw/frontend/menu.py
+++ verteilte_systeme_dhbw/frontend/menu.py
@@ -2,9 +2,7 @@ import requests
 from select_answer import *
 import os
 
-url = 'http://127.0.0.1:8000'
-url1 = 'http://127.0.0.1:8000/connect/'
-url2 = 'http://127.0.0.1:8000/question/'
+
 
 while True:
     # menu for user
@@ -14,21 +12,28 @@ while True:
     print("****************************************")
     print("\n")
     print("- You're going to get questions and you have to answer them by choosing between the options a, b or c.")
-    print("- On the top of the screen you can see the current level and the number of correct answers.")
+    print("- On the top of the screen you can see the current level and the number of correct and wrong answers.")
     print("- The difficulty level of the questions will increase with every correct answer. False answers are not "
           "going to cost you levels.")
+    print("- You can increase or decrease your level manually by pressing L.")
     print("\n")
-    user_input = input("Press S (Start) or Q(Quit)")
 
-    if user_input == "S":
-        # clear the console
-        print("\n" * 100)
-        os.system('cls' if os.name == 'nt' else 'clear')
-        start_game()
+    while True:
 
-    elif user_input == "Q":
-        print("Goodbye!")
-        exit()
+        user_input = input("Press S (Start) or Q(Quit)")
+
+        if user_input == "S":
+            # clear the console
+            print("\n" * 100)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            start_game()
+
+        elif user_input == "Q":
+            print("Goodbye!")
+            exit()
+        else:
+            print("Please enter a valid input!")
+            continue
 
 
 
