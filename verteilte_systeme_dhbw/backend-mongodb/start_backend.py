d79377766e8b321d897d86146578f1f0fb89dd46
TimoFeucht <timo@feucht-web.de>
2024-04-11 20:30:07 +0200

    bugfix: service

--- verteilte_systeme_dhbw/backend-mongodb/start_backend.py
+++ verteilte_systeme_dhbw/backend-mongodb/start_backend.py
@@ -6,3 +6,18 @@ if __name__ == "__main__":
     # use host="0.0.0.0" for Raspberry Pi
     # use host="127.0.0.1" for local development
     uvicorn.run("mongodb_app.main:app", host="0.0.0.0", port=9000, reload=True)
+
+
+# #!/usr/bin/python3
+#
+# import uvicorn
+# import subprocess
+#
+# if __name__ == "__main__":
+#     # use host="0.0.0.0" for Raspberry Pi
+#     # use host="127.0.0.1" for local development#
+#     get_ip_command = "hostname -I | awk '{print $1}'"
+#     host_ip = subprocess.check_output(get_ip_command, shell=True).decode().strip()
+#     uvicorn_command = f"uvicorn mongodb_app.main:app --reload --port 9000 --host {host_ip}"
+#
+#     subprocess.run(uvicorn_command, shell=True, check=True)
